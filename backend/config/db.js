b57ee65620b40dbb98af69cd2077dceef6cd200d
IT20146238 <it20146238@my.sliit.lk>
2022-03-31 01:58:43 +0530

    create mongo db database and connect it.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        
+      });
 
-    console.log(`MongoDB Connected: ${conn}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit();
