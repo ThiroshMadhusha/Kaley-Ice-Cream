5c924bfbf807230da9d814f4b76fdf630197eb51
IT20146238 <it20146238@my.sliit.lk>
2022-03-30 08:33:25 +0530

    test added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,16 @@ import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inventory from "./screens/InventorySystem/Inventory";
-
+import Test from "./components/TestComponent/test"
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <main>
       <Routes>
-      <Route path="/" element={<LandingPage />} exact></Route>
+        <Route path="/" element={<LandingPage />} exact></Route>
         <Route path="/inventory" element={<Inventory />}></Route>
+        <Route path="/test" element={<Test />}></Route>
       </Routes>
     </main>
     <Footer />
